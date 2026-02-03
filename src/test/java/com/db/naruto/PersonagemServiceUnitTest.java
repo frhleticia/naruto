5c8ea85c83b095c1d2b46686c96e8eaeadde5e4f
leticia.silva <frhleticia@gmail.com>
2026-02-03 08:56:08 -0300

    Testes Unitários

--- src/test/java/com/db/naruto/PersonagemServiceUnitTest.java
+++ src/test/java/com/db/naruto/PersonagemServiceUnitTest.java
@@ -4,6 +4,8 @@ import com.db.naruto.dto.PersonagemRequest;
 import com.db.naruto.model.Personagem;
 import com.db.naruto.repository.PersonagemRepository;
 import com.db.naruto.service.PersonagemService;
+import com.db.naruto.dto.JutsuRequest;
+import com.db.naruto.dto.ChakraRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -87,5 +89,82 @@ public class PersonagemServiceUnitTest {
         Personagem personagem =
                 new Personagem("Naruto", 16, "Konoha", 100);
         personagem.setId(1);
+
+        when(personagemRepository.findById(1))
+                .thenReturn(Optional.of(personagem));
+
+        personagemService.adicionarJutsu(1, new JutsuRequest("Rasengan"));
+
+        assertEquals(1, personagem.getQtdJutsus());
+        assertEquals("Rasengan", personagem.getJutsus()[0]);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoAdicionarJutsuDuplicado() {
+        Personagem personagem = new Personagem("Naruto", 16, "Konoha", 100);
+        personagem.setId(1);
+        personagem.setQtdJutsus(1);
+        personagem.getJutsus()[0] = "Rasengan";
+
+        when(personagemRepository.findById(1))
+                .thenReturn(Optional.of(personagem));
+
+        assertThrows(RuntimeException.class, () ->
+                personagemService.adicionarJutsu(1, new JutsuRequest("Rasengan")));
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoLimiteDeJutsusAtingido() {
+        Personagem personagem = new Personagem("Naruto", 16, "Konoha", 100);
+        personagem.setId(1);
+
+        personagem.setQtdJutsus(12);
+
+        when(personagemRepository.findById(1))
+                .thenReturn(Optional.of(personagem));
+
+        RuntimeException ex = assertThrows(RuntimeException.class, () ->
+                personagemService.adicionarJutsu(1, new JutsuRequest("Chidori")));
+    }
+
+    @Test
+    void deveAumentarChakraQuandoValorValido() {
+        Personagem personagem = new Personagem("Naruto", 16, "Konoha", 100);
+        personagem.setId(1);
+
+        when(personagemRepository.findById(1))
+                .thenReturn(Optional.of(personagem));
+
+        personagemService.aumentarChakra(1, new ChakraRequest(50));
+
+        assertEquals(150, personagem.getChakra());
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoAumentarChakraInvalido() {
+        ChakraRequest aumenta = new ChakraRequest(0);
+
+        assertThrows(RuntimeException.class, () ->
+                personagemService.aumentarChakra(1, aumenta));
+    }
+
+    @Test
+    void deveExibirInformacoesDoPersonagem() {
+        Personagem personagem = new Personagem("Naruto", 16, "Konoha", 100);
+        personagem.setId(1);
+
+        when(personagemRepository.findById(1))
+                .thenReturn(Optional.of(personagem));
+
+        personagemService.adicionarJutsu(1, new JutsuRequest("Rasengan"));
+
+        Personagem resultado = personagemService.buscarPersonagemPorId(personagem.getId());
+
+        assertNotNull(resultado);
+        assertEquals("Naruto", resultado.getNome());
+        assertEquals(16, resultado.getIdade());
+        assertEquals("Konoha", resultado.getAldeia());
+        assertEquals(100, resultado.getChakra());
+        assertEquals("Rasengan", resultado.getJutsus()[0]);
     }
 }
